4bdbcd43ba70cea7a11ff30b8852e6b0f3913d88
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-09-22 23:28:55 +0200

    test(node): add test for node-behavior update

--- libs/common/src/app/node/dtos/node.update.dto.spec.ts
+++ libs/common/src/app/node/dtos/node.update.dto.spec.ts
@@ -1,10 +1,57 @@
 import { plainToInstance } from "class-transformer";
 
+import { NodeBehaviorTriggerDto, NodeBehaviorVariableDto } from "./behaviors";
+import { NodeBehaviorType } from "./behaviors/node-behavior.type";
+import { NodeTriggerCronDto, NodeTriggerType } from "./behaviors/triggers";
 import { NodeKindType } from "./kind/node-kind.type";
+import { NodeCreateDto } from "./node.create.dto";
 import { NodeUpdateDto } from "./node.update.dto";
 import { transformOptions } from "../../../options";
 
 describe("NodeUpdateDto", () => {
+	describe("Behavior property", () => {
+		it("should transform `behavior=VARIABLE` correctly (partial update)", () => {
+			const toTransform = {
+				behavior: { type: NodeBehaviorType.VARIABLE },
+				kind: { active: true, type: NodeKindType.TEMPLATE },
+				name: "a node"
+			} as const satisfies NodeUpdateDto;
+
+			const transformed = plainToInstance(
+				NodeCreateDto,
+				toTransform,
+				transformOptions
+			) as typeof toTransform;
+
+			expect(transformed.behavior).toBeInstanceOf(NodeBehaviorVariableDto);
+			expect(transformed.behavior.type).toBe(toTransform.behavior.type);
+		});
+
+		it("should transform `behavior=TRIGGER` correctly (with nested trigger content)", () => {
+			const toTransform = {
+				behavior: {
+					trigger: { cron: "* * * * *", type: NodeTriggerType.CRON },
+					type: NodeBehaviorType.TRIGGER
+				},
+				kind: { active: true, type: NodeKindType.TEMPLATE },
+				name: "a node"
+			} as const satisfies NodeUpdateDto;
+
+			const transformed = plainToInstance(
+				NodeCreateDto,
+				toTransform,
+				transformOptions
+			) as typeof toTransform;
+
+			expect(transformed.behavior).toBeInstanceOf(NodeBehaviorTriggerDto);
+			expect(transformed.behavior.trigger).toBeInstanceOf(NodeTriggerCronDto);
+
+			expect(transformed.behavior.type).toBe(toTransform.behavior.type);
+			expect(transformed.behavior.trigger.type).toBe(toTransform.behavior.trigger.type);
+			expect(transformed.behavior.trigger.cron).toBe(toTransform.behavior.trigger.cron);
+		});
+	});
+
 	describe("Kind property", () => {
 		it("should transform `kind=EDGE` correctly", () => {
 			const toTransform = {
