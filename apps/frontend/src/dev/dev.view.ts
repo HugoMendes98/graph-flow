1fed9e78fbf6bc2b4d37981ca5814eb96d629848
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-10-16 10:47:16 +0200

    fix: correct frontend dev mode

--- apps/frontend/src/dev/dev.view.ts
+++ apps/frontend/src/dev/dev.view.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
 	standalone: true,
 	styleUrls: ["./dev.view.scss"],
-	templateUrl: "./dev.view.html"
+	templateUrl: "./dev.view.html",
+
+	imports: [CommonModule, RouterModule]
 })
 export class DevView {}
